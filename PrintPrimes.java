56e1c71ac4379b3dbebe171991e65890d857f45b
dmurz <murzichdm@hotmail.com>
2013-10-01 21:23:08 -0400

    Added empty lines between login blocks to increase readability

--- PrintPrimes.java
+++ PrintPrimes.java
@@ -1,4 +1,5 @@
 public class PrintPrimes {
+  
   int numberOfPrimes;   //total number of prime numbers to print
   int numberOfRows;     //number of rows per page
   int numberOfColumns;  //number of columns per page
@@ -7,18 +8,22 @@ public class PrintPrimes {
 
   //Constructor
   public PrintPrimes(int numberOfPrimes, int numberOfRows, int numberOfColumns, int ordMax) {
+
     this.numberOfPrimes   = numberOfPrimes;
     this.numberOfRows  = numberOfRows;
     this.numberOfColumns  = numberOfColumns;
     this.ordMax = ordMax;
     this.listOfPrimes = new int[numberOfPrimes + 1];
+
   }
 
   //Main method
   public static void main (String[] args) {
+
       PrintPrimes printPrimes = new PrintPrimes(300, 50, 4, 30);
       printPrimes.calculatePrimes();
       printPrimes.printPrimes();
+
   }
 
   public void calculatePrimes() {
@@ -33,61 +38,81 @@ public class PrintPrimes {
 
   //Helper function for calculatePrimes(), populates listOfPrimes[] with odd prime numbers
   private void calculateOddPrimes() {
+      
       boolean isPrime; 
       int n;
       int multiples[] = new int[ordMax + 1];
-
       int currentNumber = 1;
       int ord = 2;
       int square = 9;
 
       for (int primesFoundSoFar = 2; primesFoundSoFar <= numberOfPrimes; primesFoundSoFar++) {
+        
         do {
           currentNumber = currentNumber + 2;
+
           if (currentNumber == square) {
             ord = ord + 1;
             square = listOfPrimes[ord] * listOfPrimes[ord];
             multiples[ord - 1] = currentNumber;
           }
+
           n = 2;
           isPrime = true;
           while (n < ord && isPrime) {
+
             while (multiples[n] < currentNumber) {
               multiples[n] = multiples[n] + listOfPrimes[n] + listOfPrimes[n];
             }
+
             if (multiples[n] == currentNumber) {
               isPrime = false;
             }
+
             n = n + 1;
           }
         } while (!isPrime);
+
         listOfPrimes[primesFoundSoFar] = currentNumber;
+
       }
     }
 
     //Helper function that handles output to STDOUT
     public void printPrimes() {
+        
         int pageNumber = 1;
         int pageOffset = 1;
+
         while (pageOffset <= numberOfPrimes) {
+
           System.out.println("The First " + numberOfPrimes +
                                " Prime numbers --- Page " + pageNumber);
           System.out.println("");
+
           for (int rowOffset = pageOffset; rowOffset < pageOffset + numberOfRows; rowOffset++) {
+            
             for (int columnCounter = 0; columnCounter < numberOfColumns; columnCounter++) {
+              
               if (rowOffset + columnCounter * numberOfRows <= numberOfPrimes) {
                 System.out.format("%10d", listOfPrimes[rowOffset + columnCounter * numberOfRows]);
               }
+
             }
+
             System.out.println("");
           }
+
           System.out.println("\f");
+
           pageNumber = pageNumber + 1;
           pageOffset = pageOffset + numberOfRows * numberOfColumns;
+
         }
     }
 }
 
 					 
+	 
 
 					 
